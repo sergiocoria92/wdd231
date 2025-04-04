336e3c79a32378909311fae112662a75b4d8c46f
Sergio Coria <sergiocoria.my@gmail.com>
2025-04-03 22:15:13 -0600

    4

--- final_project/scripts/rock.js
+++ final_project/scripts/rock.js
@@ -18,3 +18,30 @@ const lastModifiedElement = document.querySelector("#last-modified");
 if (lastModifiedElement) {
     lastModifiedElement.textContent = "Última modificación: " + document.lastModified;
 }
+
+
+
+/*hamburger */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Crear el ícono de hamburguesa
+    const hamburger = document.createElement('div');
+    hamburger.classList.add('hamburger-menu');
+    for (let i = 0; i < 3; i++) {
+        const div = document.createElement('div');
+        hamburger.appendChild(div);
+    }
+
+    // Agregar el ícono de hamburguesa antes del menú
+    const header = document.querySelector('header');
+    header.appendChild(hamburger);
+
+    // Seleccionar el menú y el ícono de hamburguesa
+    const menu = document.querySelector('.nav');
+
+    // Agregar un evento de clic al ícono de hamburguesa
+    hamburger.addEventListener('click', () => {
+        // Alternar la clase 'active' para mostrar u ocultar el menú
+        menu.classList.toggle('active');
+    });
+});
